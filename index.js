8d2771c8ccfa8d8c1bf96fd44679bf92f2986005
Kosakov <nikolaikosakov@gmail.com>
2024-03-26 20:46:32 +0100

    ADD UI and adjust the main logic of the game

--- index.js
+++ index.js
@@ -1,58 +1,79 @@
+let rockBtn=document.querySelector("#rock")
+let paperBtn=document.querySelector("#paper")
+let scissorsBtn=document.querySelector("#scissors")
+let result=document.querySelector("#result")
+let playerScore=document.querySelector("#playerscore")
+let computerScore=document.querySelector("#computerscore")
+let computerChoiceField=document.querySelector("#computerChoice")
+let playerChoiceField=document.querySelector("#playerChoice")
+let resetBtn=document.querySelector("#reset")
+let welcomeText=document.querySelector("h1")
+
+
+resetBtn.onclick=()=>{
+    playerChoiceField.textContent=""
+    result.textContent=""
+    computerScore.textContent="0"
+    playerScore.textContent="0"
+    computerChoiceField.textContent=""
+    welcomeText.textContent="WELCOME"
+}
+
+rockBtn.onclick = () => {
+    playerChoiceField.textContent="You chose rock."
+    playGame(getComputerChoice(),"rock")
+};
+
+paperBtn.onclick = () => {
+    playGame(getComputerChoice(),"paper")
+    playerChoiceField.textContent="You chose paper."
+}
+
+
+scissorsBtn.onclick = () => {
+    playGame(getComputerChoice(),"scissors");
+    playerChoiceField.textContent="You chose scissors."
+}
+
+
+
 function getComputerChoice(){
     let possibleChoices=["rock","paper","scissors"]
     let randomIndex=Math.floor(Math.random() * 3);
     let finalChoice=possibleChoices[randomIndex]
+    computerChoiceField.textContent=`The mighty computer chose ${finalChoice}!`
     return finalChoice
 }
 
-//console.log(getComputerChoice())
-
-function getPlayerChoice(){
-    let message="Please type Rock,Paper or Scissors"
-    let choice=prompt(message)
-    if (!choice){
-        return 
-    }
-    let playerChoice=choice.toLowerCase()
-    if (playerChoice!="rock" && playerChoice!="paper" && playerChoice!="scissors"){
-        window.alert("Invalid Input")
-        return getPlayerChoice()
-    }
-    
-    return playerChoice
-}
-
 function playGame(Computer,Player){
-    
-    if (!Player){
-        return
-    }
-    
-
     if (Computer=="rock" && Player=="scissors" || 
     Computer=="scissors" && Player=="paper" || 
     Computer=="paper" && Player=="rock")
     {
         console.log("Computer Wins!")
-        window.alert("Computer Wins!")
+        welcomeText.textContent="DAMN"
+        result.textContent="Computer Wins!"
+        computerScore.textContent=Number(computerScore.textContent)+1
     }
 
     else if(Computer==Player)
     {
-        window.alert("It's a draw!")
+        result.textContent="It's a draw"
+        welcomeText.textContent="Not Good / Not Bad"
     }
 
 
     else{
         console.log("Player Wins!")
-        window.alert("Player Wins!")
+        welcomeText.textContent="YES BABY"
+        result.textContent="YOU WON"
+        playerScore.textContent=Number(playerScore.textContent)+1
     }
 }
 
 function playRound(){
-    for (i=0;i<5;i++){
-        playGame(getComputerChoice(),getPlayerChoice())
-    }
+   for (i=0;i<1;i++){
+      playGame(getComputerChoice(),playerChoice)
+   }
 }
 
-playRound()
